0a32064f5445ce80da3fb2298d688393a5f518a5
Valentin BARBAZA <valentin@barbaza.org>
2024-07-09 11:34:18 +0200

    Added the ssh part, worked on the organize script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 My dotfiles
+
+
+## ssh
+
+You can add any public key finishing with '.pub' to the ssh folder, it will automatically be added to 'authorized_keys' file.
--- organize.sh
+++ organize.sh
@@ -10,58 +10,89 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # bashrc
 if [ ! -z "$(which bash 2> /dev/null)" ]
 then
-  ln -fs $DIR/bashrc ~/.bashrc
+  echo "bash is installed, linking config"
+  ln -fs $DIR/bashrc $HOME/.bashrc
+else
+  echo "bash is not installed on this machine"
 fi
 
 # zshrc
 if [ ! -z "$(which zsh 2> /dev/null)" ]
 then
-  ln -fs $DIR/zshrc ~/.zshrc
+  echo "zsh is installed, linking config"
+  ln -fs $DIR/zshrc $HOME/.zshrc
+else
+  echo "zsh is not installed on this machine"
+fi
+
+##################################
+# Files in the .config directory #
+##################################
+mkdir -p $HOME/.config
+
+# kitty
+if [ ! -z "$(which kitty 2> /dev/null)" ]
+then
+  echo "Kitty is installed, linking config"
+  rm -rf $HOME/.config/kitty
+  ln -sf $DIR/kitty $HOME/.config/kitty
+else
+  echo "Kitty is not installed on this machine"
+fi
+
+####################
+# ssh config files #
+####################
+if [ ! -z "$(which ssh 2> /dev/null)" ]
+then
+  echo "ssh is installed, linking config"
+  mkdir -p $HOME/.ssh
+  ln -fs $DIR/ssh/config $HOME/.ssh/config
+  for f in ssh/*.pub
+  do
+    echo "  Adding $f to authorized keys"
+    cat $f >> $HOME/.ssh/authorized_keys
+  done
+else
+  echo "ssh is not installed on this machine"
 fi
 
 exit
 
 # vim general config
-ln -fs $DIR/vimrc ~/.vimrc
-ln -fs $DIR/vimrc.bundles ~/.vimrc.bundles
-rm -rf ~/.vim
-ln -fs $DIR/vim ~/.vim
+ln -fs $DIR/vimrc $HOME/.vimrc
+ln -fs $DIR/vimrc.bundles $HOME/.vimrc.bundles
+rm -rf $HOME/.vim
+ln -fs $DIR/vim $HOME/.vim
 
 ##################################
 # Files in the .config directory #
 ##################################
-mkdir -p ~/.config
 
 # starship prompt
-ln -fs $DIR/starship.toml ~/.config/starship.toml
+ln -fs $DIR/starship.toml $HOME/.config/starship.toml
 
-# kitty
-mkdir -p ~/.config/kitty
-ln -fs $DIR/kitty.conf ~/.config/kitty/kitty.conf
 
 # neovim
-rm -rf ~/.config/nvim
-ln -fs $DIR/nvim ~/.config/nvim
+rm -rf $HOME/.config/nvim
+ln -fs $DIR/nvim $HOME/.config/nvim
 
 # i3
-rm -rf ~/.config/i3
-ln -fs $DIR/i3 ~/.config/i3
+rm -rf $HOME/.config/i3
+ln -fs $DIR/i3 $HOME/.config/i3
 
 # Sway
-rm -rf ~/.config/sway
-ln -fs $DIR/i3 ~/.config/sway
+rm -rf $HOME/.config/sway
+ln -fs $DIR/i3 $HOME/.config/sway
 
-# kitty
-rm -rf ~/.config/kitty
-ln -fs $DIR/kitty ~/.config/kitty
 
 # polybar
-rm -rf ~/.config/polybar
-ln -fs $DIR/polybar ~/.config/polybar
+rm -rf $HOME/.config/polybar
+ln -fs $DIR/polybar $HOME/.config/polybar
 
 ###############
 # backgrounds #
 ###############
 
-# rm -rf ~/.config/bg
-ln -sf $DIR/bg ~/Pictures/
+# rm -rf $HOME/.config/bg
+ln -sf $DIR/bg $HOME/Pictures/
